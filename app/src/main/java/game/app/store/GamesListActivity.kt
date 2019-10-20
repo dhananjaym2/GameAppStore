fabbb3e49b7dbaea9f902899258e4c9a205b9bba
Dhananjay <dhananjaymohnot@gmail.com>
2019-10-21 02:00:25 +0530

    add recyclerview in games list UI via xml.

--- app/src/main/java/game/app/store/GamesListActivity.kt
+++ app/src/main/java/game/app/store/GamesListActivity.kt
@@ -1,12 +1,12 @@
 package game.app.store
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class GamesListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_games_list)
     }
 }
